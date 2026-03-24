b5c94f73d54e84af49a43137036ab6062198a602
meda28810-cpu <meda28810@gmail.com>
2026-03-24 23:12:51 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,10 @@
-self.addEventListener('install', (e) => {
- console.log('Service Worker Installed');
+self.addEventListener('install', (event) => {
+  console.log('Service Worker: Installed');
 });
 
-self.addEventListener('fetch', (e) => {
- e.respondWith(fetch(e.request));
+self.addEventListener('fetch', (event) => {
+  // هذا الكود ضروري لجعل التطبيق يعمل حتى بدون إنترنت (Offline)
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
